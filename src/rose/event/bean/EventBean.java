973609e6705cc306793ed169bbb0798eec35e395
joeykim1983 <joeykim1983@gmail.com>
2016-01-05 14:32:40 +0900

    insertevent 삭제, eventbean,typebean생성, eventsql수정

---
+++ src/rose/event/bean/EventBean.java
@@ -0,0 +1,28 @@
+package rose.event.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import rose.event.dto.EventDTO;
+
+@Controller
+public class EventBean  {
+	
+	@Autowired
+	private SqlMapClientTemplate sqlMapClient;
+	
+	@RequestMapping("/event")
+	public String event()throws Exception{
+		return "/event/event.jsp";
+	}
+	
+	@RequestMapping("/eventPro")
+	public String eventPro(EventDTO dto)throws Exception{
+		
+		sqlMapClient.insert("event.eventInsert", dto);
+		
+		return "/event/eventPro.jsp";
+	}
+}
---
+++ src/rose/event/bean/EventTypeBean.java
@@ -0,0 +1,5 @@
+package rose.event.bean;
+
+public class EventTypeBean {
+
+}

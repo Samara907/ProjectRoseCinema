8dbddd18ac33b73072d5294b33527eb4fd700b08
joeykim1983 <joeykim1983@gmail.com>
2016-01-05 14:29:41 +0900

    event,eventtype dto/bean 생성

---
+++ src/rose/event/dto/EventTypeDTO.java
@@ -0,0 +1,19 @@
+package rose.event.dto;
+
+public class EventTypeDTO {
+	private int eventtype_id;
+	private String eventtype_name;
+	
+	public int getEventtype_id() {
+		return eventtype_id;
+	}
+	public void setEventtype_id(int eventtype_id) {
+		this.eventtype_id = eventtype_id;
+	}
+	public String getEventtype_name() {
+		return eventtype_name;
+	}
+	public void setEventtype_name(String eventtype_name) {
+		this.eventtype_name = eventtype_name;
+	}
+}
